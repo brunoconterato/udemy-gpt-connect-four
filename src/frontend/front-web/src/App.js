c2df3ff8e8f80404ba788f898db98f9c9b434fe5
Bruno Conterato <brunoconterato@gmail.com>
2023-02-23 00:49:57 -0300

    feat: couse game with some fixes

--- src/frontend/front-web/src/App.js
+++ src/frontend/front-web/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AddUserForm from "./components/pages/AddUser/AddUserForm.jsx";
 import Welcome from "./components/pages/Welcome/Welcome.jsx";
 import Connect4 from "./components/pages/Connect4/Connect4";
+import Connect4Course from "./components/pages/Connect4Course/Connect4Course";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
           <Route path="/users/new" element={<AddUserForm />} />
           <Route path="/welcome/:username" element={<Welcome />} />
           <Route path="/connect4" element={<Connect4 />} />
+          <Route path="/connect4course" element={<Connect4Course />} />
         </Routes>
       </div>
     </Router>
