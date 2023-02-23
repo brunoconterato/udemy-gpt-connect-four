cb80a34cd03797364673778275a1681c008a50ac
Bruno Conterato <brunoconterato@gmail.com>
2023-02-22 22:42:34 -0300

    feat: Connect4 game generated by ChatGPT

--- src/frontend/front-web/src/App.js
+++ src/frontend/front-web/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AddUserForm from "./AddUserForm.js";
-import Welcome from "./screen/Welcome/Welcome.jsx";
-
+import AddUserForm from "./components/pages/AddUser/AddUserForm.jsx";
+import Welcome from "./components/pages/Welcome/Welcome.jsx";
+import Connect4 from "./components/pages/Connect4/Connect4";
 
 const App = () => {
   return (
@@ -10,10 +10,8 @@ const App = () => {
       <div>
         <Routes>
           <Route path="/users/new" element={<AddUserForm />} />
-          <Route
-            path="/welcome/:username"
-            element={<Welcome />}
-          />
+          <Route path="/welcome/:username" element={<Welcome />} />
+          <Route path="/connect4" element={<Connect4 />} />
         </Routes>
       </div>
     </Router>
---
+++ src/frontend/front-web/src/components/pages/Connect4/Connect4.jsx
@@ -0,0 +1,161 @@
+import React, { useState } from "react";
+import "./Connect4.css";
+
+const ROWS = 6;
+const COLS = 7;
+
+const Connect4 = () => {
+  const [board, setBoard] = useState(
+    Array.from({ length: ROWS }, () => new Array(COLS).fill(null))
+  );
+  const [player, setPlayer] = useState("red");
+  const [winner, setWinner] = useState(null);
+
+  const restartGame = () => {
+    setBoard(
+      Array.from({ length: ROWS }, () => new Array(COLS).fill(null))
+    );
+    setPlayer("red");
+    setWinner(null);
+  }
+
+  const dropCoin = (col) => {
+    // Find the first empty slot in the column
+    for (let row = ROWS - 1; row >= 0; row--) {
+      if (board[row][col] === null) {
+        // Update the board with the new move
+        const newBoard = [...board];
+        newBoard[row][col] = player;
+        setBoard(newBoard);
+
+        // Check for a win
+        if (checkWin(newBoard, player)) {
+          setWinner(player);
+          // alert(`${player} wins!`);
+          setBoard(
+            Array.from({ length: ROWS }, () => new Array(COLS).fill(null))
+          );
+          setPlayer("red");
+        } else {
+          // Switch to the other player
+          setPlayer(player === "red" ? "yellow" : "red");
+        }
+
+        // Exit the function since we found a slot
+        return;
+      }
+    }
+  };
+
+  const checkWin = (board, player) => {
+    // Check horizontal
+    for (let row = 0; row < ROWS; row++) {
+      for (let col = 0; col <= COLS - 4; col++) {
+        if (
+          board[row][col] === player &&
+          board[row][col + 1] === player &&
+          board[row][col + 2] === player &&
+          board[row][col + 3] === player
+        ) {
+          return true;
+        }
+      }
+    }
+
+    // Check vertical
+    for (let row = 0; row <= ROWS - 4; row++) {
+      for (let col = 0; col < COLS; col++) {
+        if (
+          board[row][col] === player &&
+          board[row + 1][col] === player &&
+          board[row + 2][col] === player &&
+          board[row + 3][col] === player
+        ) {
+          return true;
+        }
+      }
+    }
+
+    // Check diagonal (top left to bottom right)
+    for (let row = 0; row <= ROWS - 4; row++) {
+      for (let col = 0; col <= COLS - 4; col++) {
+        if (
+          board[row][col] === player &&
+          board[row + 1][col + 1] === player &&
+          board[row + 2][col + 2] === player &&
+          board[row + 3][col + 3] === player
+        ) {
+          return true;
+        }
+      }
+    }
+
+    // Check diagonal (bottom left to top right)
+    for (let row = 3; row < ROWS; row++) {
+      for (let col = 0; col <= COLS - 4; col++) {
+        if (
+          board[row][col] === player &&
+          board[row - 1][col + 1] === player &&
+          board[row - 2][col + 2] === player &&
+          board[row - 3][col + 3] === player
+        ) {
+          return true;
+        }
+      }
+    }
+
+    // No win found
+    return false;
+  };
+
+  const renderCoin = (row, col) => {
+    const color = board[row][col];
+    return <div className={`coin ${color}`} />;
+  };
+
+  const renderCell = (row, col) => {
+    return (
+      <div className="cell" onClick={() => dropCoin(col)} key={`${row}-${col}`}>
+        {renderCoin(row, col)}
+      </div>
+    );
+  };
+
+  const renderRow = (row) => {
+    const cells = Array.from({ length: COLS }, (_, col) =>
+      renderCell(row, col)
+    );
+    return (
+      <div className="row" key={row}>
+        {cells}
+      </div>
+    );
+  };
+
+  const renderBoard = () => {
+    const rows = Array.from({ length: ROWS }, (_, row) => renderRow(row));
+    return <div className="board">{rows}</div>;
+  };
+
+  return (
+    <div className="connect4">
+      <h1>Connect 4</h1>
+      <div className="status">
+        {player === "red" ? "Red's turn" : "Yellow's turn"}
+      </div>
+      {renderBoard()}
+      {winner && (
+        <div className="winner">
+          <div className={`winner-text ${winner}`}>
+            {winner === "red" ? "Red" : "Yellow"} wins!
+          </div>
+          <button className="restart-button" onClick={restartGame}>
+            Restart Game
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Connect4;
