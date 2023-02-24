3915ddb2b3fa63071351b018d0f0820d30845118
Bruno Conterato <brunoconterato@gmail.com>
2023-02-24 19:23:09 -0300

    feat: back to my original connect4 game only

--- src/frontend/front-web/src/App.js
+++ src/frontend/front-web/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AddUserForm from "./components/pages/AddUser/AddUserForm.jsx";
 import Welcome from "./components/pages/Welcome/Welcome.jsx";
 import Connect4 from "./components/pages/Connect4/Connect4";
-import Connect4Course from "./components/pages/Connect4Course/Connect4Course";
 
 const App = () => {
   return (
@@ -13,7 +12,6 @@ const App = () => {
           <Route path="/users/new" element={<AddUserForm />} />
           <Route path="/welcome/:username" element={<Welcome />} />
           <Route path="/connect4" element={<Connect4 />} />
-          <Route path="/connect4course" element={<Connect4Course />} />
         </Routes>
       </div>
     </Router>
